23f32d1ef80c11138ecb9c77526db2d2fa99d981
M. Scott Ford <scott@corgibytes.com>
2023-04-26 11:13:54 -0400

    Cleans up vertical and horizontal spacing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -108,13 +108,13 @@ export default function Home({ episodes }) {
           content="Conversations with the most tragically misunderstood people of our time."
         />
       </Head>
-      <div className="pb-12 pt-16 sm:pb-4 lg:pt-12">
+      <div className="pb-12 sm:pb-4">
         <Container>
           <h1 className="text-2xl font-bold leading-7 text-slate-900">
             Episodes
           </h1>
         </Container>
-        <div className="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100">
+        <div className="divide-y divide-slate-100 sm:mt-4 lg:mt-4 lg:border-t lg:border-slate-100">
           {episodes.map((episode) => (
             <EpisodeEntry key={episode.id} episode={episode} />
           ))}
