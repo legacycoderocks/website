676adcf0d426164bbb7f59d3cb0c85e09e8040cf
M. Scott Ford <scott@corgibytes.com>
2023-04-26 16:54:33 -0400

    Adds copy for the about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import { ProseContainer } from '@/components/ProseContainer'
+import { getEpisodes } from '@/api/episodes'
 
-export default function About() {
+export default function About({episodeCount}) {
   return (
     <>
       <Head>
@@ -14,15 +15,38 @@ export default function About() {
         />
       </Head>
       <ProseContainer>
-        <h2>Content goes here!</h2>
-        <p>This is a test</p>
+        <h2>About the Podcast</h2>
+        <p>
+          The Legacy Code Rocks podcast was created in 2016 in hopes that we
+          could change the way developers and others engage with and talk
+          about software systems that they inherit by others. Founders{' '} 
+          <a href="https://andreagoulet.com/">Andrea Goulet</a> and{' '}
+          <a href="https://linkedin.com/in/mscottford">M. Scott Ford</a>{' '}
+          thought that they would make an attempt by having great conversations 
+          with others who are also passionate about maintaining software.
+        </p>
+        <p>
+          So often, people use <em>legacy code</em> as a pejorative term. The
+          kind of thing that you never want to be accused of building, the
+          kind of project that no one wants to touch, the kind of codebase
+          that devs are eager to rewrite from scratch. This is the kind of
+          mentality that this show is out to shift.
+        </p>
+        <p>
+          Most episodes are less than 1 hour long. We've 
+          published {episodeCount} episodes so far, and we still have a lot more
+          that we'd like to explore and say.
+        </p>
       </ProseContainer>
     </>
   )
 }
 
 export async function getStaticProps() {
+  const episodes = await getEpisodes()
   return {
-    props: {}
+    props: {
+      episodeCount: episodes.length
+    }
   }
 }
