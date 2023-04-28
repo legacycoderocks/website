1199e52077a8e1a704244aca292f44a7259edeab
M. Scott Ford <scott@corgibytes.com>
2023-04-28 17:11:42 -0400

    Fixes link on 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,8 +8,8 @@ export default function ErrorPage() {
           <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="#"
-              className="rounded-md bg-brand-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              href="/"
+              className="rounded-md bg-brand-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-brand-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-red-600"
             >
               Go back home
             </a>
